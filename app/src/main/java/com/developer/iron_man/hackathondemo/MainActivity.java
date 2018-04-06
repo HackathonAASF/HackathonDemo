0bc8045d8b5819eb897ad04f024b57e288b8e764
archit2693 <archit2693.aj@gmail.com>
2018-04-06 21:57:22 +0530

    Button Added

--- app/src/main/java/com/developer/iron_man/hackathondemo/MainActivity.java
+++ app/src/main/java/com/developer/iron_man/hackathondemo/MainActivity.java
@@ -2,12 +2,26 @@ package com.developer.iron_man.hackathondemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView t;
+    private Button g;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        g =  findViewById(R.id.goodbye);
+        t =  findViewById(R.id.text);
+        g.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t.setText("Clicked !");
+            }
+        });
     }
 }
